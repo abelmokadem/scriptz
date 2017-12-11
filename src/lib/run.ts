44c55958c167cd60e9418ebe0f50d03427631067
Ash Belmokadem <a.belmokadem@gmail.com>
2017-12-11 12:00:23 +0100

    Read env from root as well

--- src/lib/run.ts
+++ src/lib/run.ts
@@ -1,6 +1,7 @@
 import { IRunConfiguration } from "./run-configuration.interface";
 import { Observable } from "rxjs/Observable";
 import { runFlow } from "./run-flow";
+import * as path from "path";
 
 export const run = (configuration: IRunConfiguration): Observable<any> => {
   const options: any = configuration.options || {};
@@ -9,7 +10,12 @@ export const run = (configuration: IRunConfiguration): Observable<any> => {
     typeof options.retry_count !== "undefined"
       ? parseInt(options.retry_count, 10)
       : 0;
-  options.env = typeof options.env !== "undefined" ? options.env : {};
+
+  const env =
+      typeof options.env === "string"
+          ? require(path.join(process.cwd(), options.env)).env
+          : options.env;
+  options.env = Object.assign({}, env);
 
   console.log(
     `Running scripts with the following options: \n${JSON.stringify(
